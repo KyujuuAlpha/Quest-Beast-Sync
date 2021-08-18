7fd9ef2ee8dd3c194f5778b66303611a6a81e730
KyujuuAlpha <troiryans@gmail.com>
2021-08-17 19:43:30 -0700

    experiments with downloading

--- sync.py
+++ sync.py
@@ -9,6 +9,10 @@ import os.path
 BSABER_API_BOOKMARKS_URL = "https://bsaber.com/wp-json/bsaber-api/songs/?bookmarked_by="
 BSAVER_API_DOWNLOAD_URL = "https://api.beatsaver.com/download/key/"
 
+# Folder constants
+# CUSTOM_LEVEL_FOLDER = "/sdcard/ModData/com.beatgames.beatsaber/Mods/SongLoader/CustomLevels"
+CUSTOM_LEVEL_FOLDER = "./CustomLevels"
+
 # retrieve bookmarked songs for BeastSaber user for a specified page
 # {"title":"Darling Dance(YuNi Cover) - Kairiki Bear","song_key":"1b49e","hash":"57431519f148298175a53720b68907a6f4a52456","level_author_name":"Emir"}
 def get_bsaber_songs(user, page):
@@ -25,36 +29,24 @@ def get_bsaber_songs(user, page):
 
     # return the data
     return bsaber_json["songs"], bsaber_json["next_page"]
-    
-def download_songs(songs):
-    # move on to beat saver
-    print("Downloading songs from BeatSaver..")
-
-    # download the songs
-    # for i in range(0, len(download_links)):
-    #     # get current download stuff
-    #     name = download_names[i]
-    #     link = download_links[i]
-
-    #     # download logic
-    #     if link.find("beatsaver") != -1:
-    #         if os.path.isfile(name + ".zip") == False:
-    #             print("Downloading from: " + link)
-    #             wget.download(link, name + ".zip")
-    #             print()
-    #             time.sleep(random.randint(1, 3))
-    #         else:
-    #             number_songs_zero = number_songs_zero + 1
-    #             print("Song " + name + ".zip already downloaded, skipping..")
-    #     else:
-    #         number_songs_zero = number_songs_zero + 1
-    #         print("Not a BeatSaver link, skipping song..")
-
-    #     # timing logic
-    #     time_spent = time_spent + (round(time.time() * 1000) - prev_time)
-    #     prev_time = round(time.time() * 1000)
 
 if __name__ == "__main__":
     bsaber_user = "enthri"
+    
+    if os.path.exists(CUSTOM_LEVEL_FOLDER) == False:
+        print("Folder does not exist, creating..")
+        os.mkdir(CUSTOM_LEVEL_FOLDER)
+    
     songs, next_page = get_bsaber_songs(bsaber_user, 1)
+    if songs != None:
+        for song in songs:
+            song_url = BSAVER_API_DOWNLOAD_URL + song["song_key"]
+            download_location = CUSTOM_LEVEL_FOLDER + "/" + song["hash"] + ".zip"
+            if os.path.exists(download_location) == False:
+                wget.download(song_url, download_location)
+            else:
+                print("Song exists..")
+    else:
+        print("Songs not found!")
+        exit(1)
     print(next_page)
