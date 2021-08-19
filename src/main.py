17add6a7c3094b4807531ec3a8d6185c3e2b9e82
KyujuuAlpha <troiryans@gmail.com>
2021-08-18 18:16:59 -0700

    threaded downloads

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivy.clock import mainthread
+from kivy.properties import NumericProperty
 from android.permissions import Permission, request_permissions
 
 import sync
 import threading
+import math
+import time
+
+DOWNLOAD_THREAD_COUNT = 6
+THREAD_SPAWN_DELAY = 1
 
 # main root widget
 class MainScreen(MDScreen):
+
+    download_counter = 0
+    download_max = 0
+
     @mainthread
     def set_in_progress(self, disabled):
         # lock the ui according to the disabled parameter
@@ -20,10 +30,23 @@ class MainScreen(MDScreen):
         # set the status of the downloading
         self.status.text = text
 
-    def download_thread(self):
-        # check if path exists
-        sync.safe_path_check()
+    @mainthread
+    def status_counter_increase(self):
+        self.download_counter += 1
+        self.status.text = "Downloaded " + str(self.download_counter) + " of " + str(self.download_max)
+    
+    @mainthread
+    def status_counter_reset(self, max):
+        self.download_counter = 0
+        self.download_max = max
+        self.status.text = "Downloaded " + str(self.download_counter) + " of " + str(self.download_max)
+    
+    def song_download_worker(self, songs):
+        for song in songs:
+            sync.download_song(song)
+            self.status_counter_increase()
 
+    def download_thread(self):
         # retrieve user
         bsaber_user = self.user.text
 
@@ -36,13 +59,25 @@ class MainScreen(MDScreen):
 
         # if there are songs, then download and extract them
         if songs != None and num_songs > 0:
-            for i in range(0, num_songs):
-                self.set_status_text("Downloading song " + str(i + 1) + " of " + str(num_songs))
-                song = songs[i]
-                try:
-                    sync.download_song(song)
-                except Exception as err:
-                    self.set_status_text("uh oh")
+            threads = []
+            self.status_counter_reset(num_songs)
+            if num_songs >= DOWNLOAD_THREAD_COUNT:
+                chunk_size = math.floor(num_songs / DOWNLOAD_THREAD_COUNT)
+                for i in range(0, DOWNLOAD_THREAD_COUNT):
+                    worker_song_list = []
+                    if i == DOWNLOAD_THREAD_COUNT - 1:
+                        worker_song_list = songs[(chunk_size * i):]
+                    else:
+                        worker_song_list = songs[(chunk_size * i):(chunk_size * (i + 1))]
+                    threads.append(threading.Thread(target = (self.song_download_worker), args=[worker_song_list]))
+                    threads[-1].start()
+                    time.sleep(THREAD_SPAWN_DELAY)
+            else:
+                threads.append(threading.Thread(target = (self.song_download_worker), args=[songs]))
+                threads[-1].start()
+            
+            for thread in threads:
+                thread.join()
 
             self.set_status_text("Songs synchronized!")
         else:
@@ -55,6 +90,9 @@ class MainScreen(MDScreen):
         if len(self.user.text) == 0:
             self.set_status_text("Please enter BeastSaber username!")
             return
+        
+        # check if path exists
+        sync.safe_path_check()
         
         self.set_in_progress(True)
         threading.Thread(target = (self.download_thread)).start()
